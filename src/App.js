8fdf3265198dc12ea7630258a6075df74a78f9e6
Tebogo Dikgatlong <tpdikgatlong@gmail.com>
2024-09-13 19:15:54 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LPTB SOLUTIONS</h1>
+      <h2>Web Application</h2>
+    </div>
+  );
+}
+
+export default App;
