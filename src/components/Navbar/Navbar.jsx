4248b0c39959746a36b70314c1c04c1941de0f05
Tebogo Dikgatlong <tpdikgatlong@gmail.com>
2024-09-24 14:55:49 +0200

    Working on Navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,59 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiFillCloseSquare } from "react-icons/ai"
 
 import { Link } from 'react-router-dom'
 
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-        <div className="nav-larger">
-            <div className='logo'>
-                <Link to='/'>
-                    <h1>LPTB</h1>
-                </Link>
-            </div>
-            
-            <ul className='nav-links'>
-                <li><Link className='links' to='/'>Home</Link></li>
-                <li><Link className='links' to='/about'>About</Link></li>
-                <li><Link className='links' to='/services'>Services</Link></li>
-                <li><Link className='links' to='/webdev'>Web Dev</Link></li>
-                <li><Link className='links' to='/contact'>Contact</Link></li>
-            </ul>
+    const [toggleMenu, setToggleMenu] = useState(false)
 
-            <div className='account'>
-                <Link className='links' to='/login'>Log In</Link>
-                <Link className='links' to='/signup'>Sign Up</Link>
-            </div>
-        </div>
+    return (
+        <nav className='navbar'>
+            <div className="nav-larger">
+                <div className='logo'>
+                    <Link to='/'>
+                        <h1>LPTB</h1>
+                    </Link>
+                </div>
+                
+                <ul className='nav-links'>
+                    <li><Link className='links' to='/'>Home</Link></li>
+                    <li><Link className='links' to='/about'>About</Link></li>
+                    <li><Link className='links' to='/services'>Services</Link></li>
+                    <li><Link className='links' to='/webdev'>Web Dev</Link></li>
+                    <li><Link className='links' to='/contact'>Contact</Link></li>
+                </ul>
 
-        
-        
-        <div className="nav-links_small-screens">
-        <div className='logo-small'>
-                <Link to='/'>
-                    <h1>LPTB</h1>
-                </Link>
+                <div className='account'>
+                    <Link className='links' to='/login'>Log In</Link>
+                    <Link className='links' to='/signup'>Sign Up</Link>
+                </div>
             </div>
-            <GiHamburgerMenu className='hamburger-menu'/>
 
-            <ul className='nav-links_small'>
-                <li><Link className='links' to='/'>Home</Link></li>
-                <li><Link className='links' to='/about'>About</Link></li>
-                <li><Link className='links' to='/services'>Services</Link></li>
-                <li><Link className='links' to='/webdev'>Web Dev</Link></li>
-                <li><Link className='links' to='/contact'>Contact</Link></li>
-            </ul>
+            <div className="nav-links_small-screens">
+                <GiHamburgerMenu className='hamburger-menu' onClick={() => setToggleMenu(true)} />
 
-            <div className='account-small'>
-                <Link className='links' to='/login'>Log In</Link>
-                <Link className='links' to='/signup'>Sign Up</Link>
+
+                {toggleMenu && (
+                    <div className='navlinks-small_overlay'>
+                        <AiFillCloseSquare className='hamburger-menu close' onClick={() => setToggleMenu(false)} />
+                        <div className='logo-small'>
+                            <Link onClick={() => setToggleMenu(false)} to='/'>
+                                <h1>LPTB</h1>
+                            </Link>
+                        </div>
+                        
+
+                        <ul className='nav-links_small'>
+                            <li onClick={() => setToggleMenu(false)}><Link className='links' to='/'>Home</Link></li>
+                            <li onClick={() => setToggleMenu(false)}><Link className='links' to='/about'>About</Link></li>
+                            <li onClick={() => setToggleMenu(false)}><Link className='links' to='/services'>Services</Link></li>
+                            <li onClick={() => setToggleMenu(false)}><Link className='links' to='/webdev'>Web Dev</Link></li>
+                            <li onClick={() => setToggleMenu(false)}><Link className='links' to='/contact'>Contact</Link></li>
+                        </ul>
+
+                        <div className='account-small'>
+                            <Link onClick={() => setToggleMenu(false)} className='links' to='/login'>Log In</Link>
+                            <Link onClick={() => setToggleMenu(false)} className='links' to='/signup'>Sign Up</Link>
+                        </div>
+                    </div>
+                )}
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
